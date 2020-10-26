ef252cf833d5f5f0827356fef02cc45db0eb152a
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-10-26 14:16:16 +0100

    Påbörjad kod av hänga gubbe
    allt som jag kommer ihåg så länge

--- Hänga gubbe prövning/Program.cs
+++ Hänga gubbe prövning/Program.cs
@@ -4,9 +4,40 @@ namespace Hänga_gubbe_prövning
 {
     class Program
     {
+        int gissningar = 30;
+        string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("välkommen till hänga gubbe spelet som kanske räddar mitt betyg");
+            Console.WriteLine("");
+            Console.WriteLine("Du kommer att ha 30 gissningar");
+
+            string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
+            int gissningar = 30;
+
+            do
+            {
+                
+
+
+
+            }
+            while (gissningar > 0);
+
+
+        }
+        static void Visaordet()
+        {
+
+        }
+
+        static void Random()
+        {
+            string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
+
+            Random rand = new Random();
+
+
         }
     }
 }
