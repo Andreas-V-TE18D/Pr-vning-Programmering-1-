6c717855c9f7385c9c039242455b5e3bbe5d3c63
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-10-27 10:41:46 +0100

    gjort random metod

--- Hänga gubbe prövning/Program.cs
+++ Hänga gubbe prövning/Program.cs
@@ -1,43 +1,57 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace Hänga_gubbe_prövning
 {
     class Program
     {
-        int gissningar = 30;
-        string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
+        static int gissningar = 30;
+        static string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
         static void Main(string[] args)
         {
             Console.WriteLine("välkommen till hänga gubbe spelet som kanske räddar mitt betyg");
             Console.WriteLine("");
             Console.WriteLine("Du kommer att ha 30 gissningar");
 
-            string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
-            int gissningar = 30;
+            // tar ut ett slumpmässigt ord från array listan
+            Random rand = new Random () ;
+            int randomtal = rand.Next(3);
+            string ordet = ord[randomtal];
 
-            do
+            
+
+
+            string gissning = Console.ReadLine();
+
+            char[] bokstav = gissning.ToCharArray();
+
+
+            while (gissningar > 0)
             {
-                
+                if (ordet.Contains(gissning))
+                {
+                    Console.WriteLine("RÄTT GISSAT");
+                    Console.WriteLine(bokstav);
 
+                }
+
+                else
+                {
+                    Console.WriteLine("FEL GISSAT");
+
+                }
 
 
             }
-            while (gissningar > 0);
+            if (gissningar < 0)
+            {
+                Console.WriteLine("Game Over");
+            }
 
 
-        }
-        static void Visaordet()
-        {
-
-        }
-
-        static void Random()
-        {
-            string[] ord = new string[4] { "andreas", "david", "alexander", "mikael", };
-
-            Random rand = new Random();
-
+           
 
         }
+      
     }
 }
