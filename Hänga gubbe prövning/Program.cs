d33be1c959088e10482b3841b3f01aef670d0e0e
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-10-28 09:53:03 +0100

    Klar med basic spelet

--- Hänga gubbe prövning/Program.cs
+++ Hänga gubbe prövning/Program.cs
@@ -11,45 +11,74 @@ namespace Hänga_gubbe_prövning
         {
             Console.WriteLine("välkommen till hänga gubbe spelet som kanske räddar mitt betyg");
             Console.WriteLine("");
-            Console.WriteLine("Du kommer att ha 30 gissningar");
+            Console.WriteLine("Spelet går ut på att gissa vilka bokstäver finns i ett hemligt ord");
 
             // tar ut ett slumpmässigt ord från array listan
-            Random rand = new Random () ;
-            int randomtal = rand.Next(3);
+            Random random = new Random () ;
+            int randomtal = random.Next(4);
             string ordet = ord[randomtal];
 
-            
 
+            // representationen för ordet 
+            char[] stars = new char[ordet.Length];
 
-            string gissning = Console.ReadLine();
-
-            char[] bokstav = gissning.ToCharArray();
-
-
-            while (gissningar > 0)
+            for (int i = 0; i < stars.Length; i++)
             {
+                stars[i] = '*';
+            }
+
+            // frågar användaren om hur många gissningar man vill ha 
+            Console.WriteLine("Hur många gissningar vill du ha?");
+            int antalGissningar = Convert.ToInt32(Console.ReadLine());
+
+            // do while loopen körs så länge antalet gissningar är större än 0 
+            do
+            {
+                Console.WriteLine("Gissa en bokstav");
+                string gissning = Console.ReadLine();
+
                 if (ordet.Contains(gissning))
                 {
-                    Console.WriteLine("RÄTT GISSAT");
-                    Console.WriteLine(bokstav);
+                    char[] ordChars = ordet.ToCharArray();
+
+                    for (int i = 0; i < ordChars.Length; i++)
+                    {
+                        if (gissning.ToCharArray()[0] == ordChars[i])
+                        {
+                            stars[i] = gissning.ToCharArray()[0];
+                        }
+                    }
+
+                    Console.WriteLine(stars);
+                    string starWord = new string(stars);
+
+                    if (starWord.Contains('*'))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du vann");
+                        break;
+                    }
 
                 }
 
                 else
                 {
-                    Console.WriteLine("FEL GISSAT");
+                    Console.WriteLine("Fell gissning");
+                    antalGissningar--;
+                    Console.WriteLine("Du har: " + antalGissningar + "kvar");
 
                 }
 
-
-            }
-            if (gissningar < 0)
-            {
-                Console.WriteLine("Game Over");
-            }
+            } while (antalGissningar > 0);
 
 
            
+
+
+
 
         }
       
